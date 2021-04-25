86728462c5fb69bdef3e28bde8f963808bf0f096
kapil jhajhria <jhajhria44@gmail.com>
2021-04-25 10:39:07 +0530

    Button Styled

--- src/Components/Users/AddUser.jsx
+++ src/Components/Users/AddUser.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
 
 export default function AddUser(props) {
   const addUserHandler = (e) => {
@@ -14,7 +15,7 @@ export default function AddUser(props) {
         <input id="username" type="text" />
         <label htmlFor="age">Age(years)</label>
         <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
